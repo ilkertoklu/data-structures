0445b59c269bbe3e3e4e20d96549d423c8a29a2d
ilkertoklu <ilkertoklu@protonomail.com>
2022-12-12 14:35:11 +0300

    recrusive

--- recrusive.c
+++ recrusive.c
@@ -2,15 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-void foo(int i)
+void foo(int n)
 {
-	if(i > 0) {
-		foo(i - 1);
-		printf("%d\n", i);
+	if(n > 0) {
+		foo(n - 1);
+		printf("%d\n", n);
   }
 }
 
+int factorialCheck(int n)
+{
+	int result = 1;
+	for (int i = n; i > 0; i--)
+		result = result * i;
+	return result;
+}
+
+int factorial(int n)
+{
+	if (n == 1)
+		return 1;
+	return n * factorial(n-1);
+}
+
+
+
 int main()
 {
-	foo(3);
+	printf("%d\n", factorialCheck(5));
+	printf("%d\n", factorial(5));
 }
+
